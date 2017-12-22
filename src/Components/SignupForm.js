096dd88dd6bb8b886e8edb85ba7ea48a6376e38b
Nick Foden <nickfoden@gmail.com>
2017-12-22 16:25:52 -0500

    Broke the user signup.

--- src/Components/SignupForm.js
+++ src/Components/SignupForm.js
@@ -11,10 +11,10 @@ const constraints = {
 }
 
 class SignupForm extends Component {
-   
+   git
     submit(values){
         console.log(values.username)
-        return axios.post(`${API_BASE_URL}`,{
+        return axios.post(`${API_BASE_URL}/user/${values.username}`,{
             username : values.username
         } ).then(results => {
             console.log(results);
@@ -38,23 +38,3 @@ export default reduxForm({
     form: 'SignupForm',
     validate: formValidator(constraints)
 })(SignupForm);
-
-// <input type="text" name="firstName"></input>
-// <input
-//     type="text"
-    
-//     >
-// </input>
-// <input name="criticalLevel">
-//     <option />
-//     <option value="#00ff00">Green</option>
-//     <option value="#f3f977">Yellow</option>
-//     <option value="#ff0000">Red</option>
-// </input>
-// <input type="textarea">
-// </input> 
-// <button
-//     type="submit"
-//     disabled={this.props.pristine || this.props.submitting}>
-//     Register
-// </button>
