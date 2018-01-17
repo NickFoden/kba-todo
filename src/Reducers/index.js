e31434e38ceafc07bd4f4571a877bdf57fece791
Nick Foden <nickfoden@gmail.com>
2018-01-17 12:36:22 -0500

    updating redux store to use middleware

--- src/Reducers/index.js
+++ src/Reducers/index.js
@@ -1,10 +1,6 @@
-import { combineReducers } from "redux";
-import { reducer as reduxFormReducer } from "redux-form";
-import todos from "./todos";
+import { combineReducers } from 'redux';
+import todos from './todos';
 
-const rootReducer = combineReducers({
-  todos,
-  form: reduxFormReducer
+export default combineReducers({
+  todos: () => [],
 });
-
-export default rootReducer;
--- src/store.js
+++ src/store.js
@@ -1,22 +1,14 @@
 import { createStore, applyMiddleware, compose } from 'redux';
-import rootReducer from './Reducers'
-import thunk from 'redux-thunk'
+import thunk from 'redux-thunk';
+import rootReducer from './Reducers';
 
-const store = createStore(
-    rootReducer,
-    compose(
-      applyMiddleware(thunk),
-    window.devToolsExtension ? window.devToolsExtension() : f => f)
-  )
+const middlewares = [thunk];
 
-  window.store = store;
-
-export default store
-
-//   const authToken = loadAuthToken();
-//   if (authToken) {
-//       const token = authToken;
-//       store.dispatch(setAuthToken(token));
-//   }
-
-//END OF PAGE
+export default createStore(
+  rootReducer,
+  undefined,
+  compose(
+    applyMiddleware(...middlewares),
+    window.devToolsExtension ? window.devToolsExtension() : f => f,
+  ),
+);
